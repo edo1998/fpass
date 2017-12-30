d0c2e07163ecfc900ee290a71317a5a593b822f7
yanez63 <roberto.tolotti@pentasistemi.com>
2017-12-30 16:15:52 +0100

    Creati primi file

---
+++ server-side/nuovo.php
@@ -0,0 +1,7 @@
+<?
+//la stringa mysql_connect deve essere compilata con i dati relativi al proprio database
+// HOST = IP server Mysql
+// USER = Nome utente databse
+// PASSWORD = Password utente databse
+mysql_connect("localhost:8889","root","root");//database connection
+?>
