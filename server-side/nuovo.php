b76202c22dc1f066d79a03649a32c3ccd8c22c28
yanez63 <roberto.tolotti@pentasistemi.com>
2018-01-04 07:55:46 +0100

    avanzamento

--- server-side/nuovo.php
+++ server-side/nuovo.php
@@ -1,46 +1,42 @@
 <?php
 
-class F-Pass_DB {
+class F_Pass_DB {
     
     public $host = "localhost";
     public $user = "root";
     public $password = "root";
     public $db = "F-Pass";
-    public $mysqli;
+    private $F_pass_db;
 
-        // costruttore
-        public function __construct() {
+    // costruttore
+    public function __construct() {
 
-            $this->mysqli = new mysqli($this->host, $this->user, $this->password, $this->db);
+        $this->mysqli = new mysqli($this->host, $this->user, $this->password, $this->db);
 
-            /* check connection */
-            if (mysqli_connect_errno()) {
-                printf("Connect failed: %s\n", mysqli_connect_error());
-                exit();
-            } else{
-                echo("<br>Connessione OK");
-            }         
-        }
+        /* check connection */
+        if (mysqli_connect_errno()) {
+            printf("Connect failed: %s\n", mysqli_connect_error());
+            exit();
+        } else{
+            echo("<br>Connessione OK");
+        }         
+    }
         
-        // distruttore
-        public function __destruct() {
-            /* close connection */
-            $this->mysqli->close();
-        }
-         
-        
-        
-        
-        
+    // distruttore
+    public function __destruct() {
+        /* close connection */
+        $this->mysqli->close();
+    }
+
+    public function query($query) {
+    
+        $result = $this->query($query);
+
+        return $result;
+    }           
         
 }
 
-
-
-
-
-
-
-
+$DataBase = new F_Pass_DB();
 
 ?>
