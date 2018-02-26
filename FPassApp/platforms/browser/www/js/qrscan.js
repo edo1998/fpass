c76d88211ded816680aea9988176d83e116375b7
edo1998 <edo.iscrizioni@rinonline.com>
2018-02-26 19:40:04 +0100

    modifiche

--- FPassApp/platforms/browser/www/js/qrscan.js
+++ FPassApp/platforms/browser/www/js/qrscan.js
@@ -15,30 +15,25 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        document.getElementById("body").style.visibility = "visible";
-    },
+		function displayContents(err, text){
+			console.log("!");
+			if(err){
+				console.log(JSON.stringify(err))
+		    	// an error occurred, or the scan was canceled (error code `6`)
+			} else {
+		    	// The scan completed, display the contents of the QR code:
+				console.log(JSON.stringify(text));
+			}
+		}
+
+		QRScanner.scan(displayContents);
+
+		// Make the webview transparent so the video preview is visible behind it.
+		QRScanner.show(function(status){
+			console.log(status);
+		});
+		// Be sure to make any opaque HTML elements transparent here to avoid
+		// covering the video.
+    }
     // Update DOM on a Received Event
 };
-
-// Start a scan. Scanning will continue until something is detected or
-// `QRScanner.cancelScan()` is called.
-function scan(){
-
-	var QRScanner = window.cordova.require('QRScanner');
-
-	function displayContents(err, text){
-	  if(err){
-	    // an error occurred, or the scan was canceled (error code `6`)
-	  } else {
-	    // The scan completed, display the contents of the QR code:
-	    alert(text);
-	  }
-	}
-
-	QRScanner.scan(displayContents);
-
-	// Make the webview transparent so the video preview is visible behind it.
-	QRScanner.show();
-	// Be sure to make any opaque HTML elements transparent here to avoid
-	// covering the video.
-}
