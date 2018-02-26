14bd5ba34c710bc77e99f390c8a9364e65f9993f
yanez63 <roberto.tolotti@pentasistemi.com>
2018-02-26 21:06:09 +0100

    aggiunto array json

--- server-side/legge_anag.php
+++ server-side/legge_anag.php
@@ -12,18 +12,22 @@ $result = $mydb->clear_query();
 $mydb->item_type[0] = "s";
 $mydb->item_type[1] = "s";
 
+
 $mydb->item_value[0] = $_POST["mail"];
 $mydb->item_value[1] = $_POST["passcode"];
 
-$mydb->comando = "SELECT Cognome,Nome FROM anag WHERE Mail = ? or PassCode = ?";
+$mydb->comando = "SELECT Cognome,Nome,PassCode FROM anag WHERE Mail = ? or PassCode = ?";
 
 $rows = $mydb -> query();
+
 
 if ($rows == 0) {
     echo "Lettura fallita";
     } else {
-        echo $rows[0]['Cognome'];
-        echo $rows[0]['Nome'];  
+        $dati['cognome'] = $rows[0]['Cognome'];
+        $dati['nome'] = $rows[0]['Nome'];
+        $dati['passcode'] = $rows[0]['PassCode'];
+        echo json_encode($dati);
     }
     
 ?>
