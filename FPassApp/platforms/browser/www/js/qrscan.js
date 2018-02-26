4348c9ad24f7df08db3cf6ef63fd0a7c4edb5886
edo1998 <edo.iscrizioni@rinonline.com>
2018-02-26 21:32:46 +0100

    Modifiche

--- FPassApp/platforms/browser/www/js/qrscan.js
+++ FPassApp/platforms/browser/www/js/qrscan.js
@@ -14,24 +14,23 @@ var app = {
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+	onDeviceReady: function() {
 		function displayContents(err, text){
-			console.log("!");
 			if(err){
-				console.log(JSON.stringify(err))
+				console.log(JSON.stringify(err));
 		    	// an error occurred, or the scan was canceled (error code `6`)
 			} else {
 		    	// The scan completed, display the contents of the QR code:
 				console.log(JSON.stringify(text));
 			}
 		}
-
-		QRScanner.scan(displayContents);
-
+		console.log(JSON.stringify(window))
+		//window.QRScanner.scan(displayContents);
+		console.log("2");
 		// Make the webview transparent so the video preview is visible behind it.
-		QRScanner.show(function(status){
-			console.log(status);
-		});
+		//window.QRScanner.show(function(status){
+			//console.log(status);
+		//});
 		// Be sure to make any opaque HTML elements transparent here to avoid
 		// covering the video.
     }
---
+++ FPassApp/platforms/ios/www/js/registrazione.js
@@ -0,0 +1,63 @@
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        document.getElementById("body").style.visibility = "visible";
+    },
+    // Update DOM on a Received Event
+};
+
+function ValidateEmail(mail)
+{
+ if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+  {
+    return true;
+  }
+    return false;
+}
+
+
+function validate() {
+	var nome = document.getElementById('nome').value;
+	var cognome = document.getElementById('cognome').value;
+	var mail = document.getElementById('mail').value;
+	var password = document.getElementById('password').value;
+	var conf_password = document.getElementById('conf_password').value;
+	var check_mail = ValidateEmail(mail);
+	if(check_mail && (password == conf_password && password != "") && nome != "" && cognome != ""){
+		var oggetto ={ nome: nome, cognome: cognome,  mail: mail, password: password};
+		$.post(
+			"https://www.rinonline.com/fpass/nuovo_anag.php",
+			oggetto
+		);
+		return false;
+	}else{
+		if (!check_mail){
+			document.getElementById("mail").className += "error";
+		}
+		if (password != conf_password || password == ""){
+			document.getElementById("password").className += "error";
+			document.getElementById("conf_password").className += "error";
+		}
+		if (nome == ""){
+			document.getElementById("nome").className += "error";
+		}
+		if (cognome == ""){
+			document.getElementById("cognome").className += "error";
+		}
+		return false;
+	}
+}
