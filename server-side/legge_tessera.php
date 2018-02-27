49a3cf45a390711ca7154c9e4aa919661bdc674c
yanez63 <roberto.tolotti@pentasistemi.com>
2018-02-27 21:32:34 +0100

    corretto

--- server-side/legge_tessera.php
+++ server-side/legge_tessera.php
@@ -15,7 +15,7 @@ $mydb->item_type[1] = "s";
 $mydb->item_value[0] = $_POST["PassCode"];
 $mydb->item_value[1] = $_POST["locale"];
 
-$mydb->comando = "SELECT codice, tipo FROM codice WHERE PassCode = ? and id_locale = ?";
+$mydb->comando = "SELECT codice, tipo FROM codici WHERE PassCode = ? and id_locale = ?";
 
 $rows = $mydb -> query();
 
