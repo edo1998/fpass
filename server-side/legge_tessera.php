c43f6462064c3db500e8e1118c341892ba8d42d3
yanez63 <roberto.tolotti@pentasistemi.com>
2018-02-27 21:30:43 +0100

    aggiunto legge_tessera

---
+++ server-side/legge_tessera.php
@@ -0,0 +1,32 @@
+
+<?php
+
+
+include('MyDB.php');
+
+$mydb = new DB();
+ 
+$result = $mydb->clear_query();
+
+$mydb->item_type[0] = "s";
+$mydb->item_type[1] = "s";
+
+
+$mydb->item_value[0] = $_POST["PassCode"];
+$mydb->item_value[1] = $_POST["locale"];
+
+$mydb->comando = "SELECT codice, tipo FROM codice WHERE PassCode = ? and id_locale = ?";
+
+$rows = $mydb -> query();
+
+if ($rows == 0) {
+        $dati['errore'] = 1;
+        echo json_encode($dati);
+    } else {
+        $dati['codice'] = $rows[0]['codice'];
+        $dati['tipo'] = $rows[0]['tipo'];
+        $dati['errore'] = 0;
+        echo json_encode($dati);
+    }
+    
+?>
