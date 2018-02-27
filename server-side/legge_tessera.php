46ffba967f220bf63fc39d2de2dc25f9d36c40be
yanez63 <roberto.tolotti@pentasistemi.com>
2018-02-27 21:40:09 +0100

    aggiunta descrizione locale

--- server-side/legge_tessera.php
+++ server-side/legge_tessera.php
@@ -15,7 +15,7 @@ $mydb->item_type[1] = "s";
 $mydb->item_value[0] = $_POST["PassCode"];
 $mydb->item_value[1] = $_POST["locale"];
 
-$mydb->comando = "SELECT codice, tipo FROM codici WHERE PassCode = ? and id_locale = ?";
+$mydb->comando = "SELECT codice, tipo, descrizione FROM codici, locali WHERE PassCode = ? and id_locale = ?";
 
 $rows = $mydb -> query();
 
@@ -25,6 +25,7 @@ if ($rows == 0) {
     } else {
         $dati['codice'] = $rows[0]['codice'];
         $dati['tipo'] = $rows[0]['tipo'];
+        $dati['descrizione'] = $rows[0]['descrizione'];
         $dati['errore'] = 0;
         echo json_encode($dati);
     }
