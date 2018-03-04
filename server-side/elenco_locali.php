9a8b2d08d2f3077a57c47a1137e534baf3708aa8
edo1998 <edo.iscrizioni@rinonline.com>
2018-03-04 15:56:44 +0100

    Aggiunto id della combobox

--- server-side/elenco_locali.php
+++ server-side/elenco_locali.php
@@ -5,7 +5,7 @@
 include('MyDB.php');
 
 $mydb = new DB();
- 
+
 $result = $mydb->clear_query();
 
 $mydb->comando = "SELECT id, descrizione FROM locali order by descrizione";
@@ -16,13 +16,14 @@ if ($rows == 0) {
         $dati['errore'] = 1;
         echo json_encode($dati);
     } else {
-        echo "<select>";
-        
+        echo '<select name="id_locale" id="id_locale">';
+		echo '<option value="" selected disabled>Scegli la tessera</option>';
+
         for ($x = 0; $x <= count($rows); $x++) {
             echo "<option value='{$rows[$x][id]}'>{$rows[$x][descrizione]}</option>";
-        } 
-        
+        }
+
         echo "</select>";
     }
-    
+
 ?>
