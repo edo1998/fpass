791775879d76b0704f2427d1f179f5eb6db80f7c
yanez63 <roberto.tolotti@pentasistemi.com>
2018-03-04 18:03:11 +0100

    aggiunto id

--- server-side/elimina_tessera.php
+++ server-side/elimina_tessera.php
@@ -14,15 +14,8 @@ $mydb->item_type[1] = "s";
 $mydb->item_value[0] = $_POST["passcode"];
 $mydb->item_value[1] = $_POST["id_locale"];
 
-$mydb->comando = "DELETE FROM codici WHERE Passcode = ? and id_locale = ?)";
+$mydb->comando = "DELETE FROM codici WHERE Passcode = ? and id_locale = ?";
 
 $result = $mydb -> query();
 
-if ($result <> 0) {
-        $dati['errore'] = 1;
-        echo json_encode($dati);
-    } else {
-        $dati['errore'] = 0;
-        echo json_encode($dati);
-    }
 ?>
